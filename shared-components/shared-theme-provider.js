7d1269282ffe3846aa6e6ff6c24dbfac1f036acd
Paul McClean <pmcclean@petsafe.net>
2022-01-24 12:32:15 +0000

    Update comments

--- shared-components/shared-theme-provider.js
+++ shared-components/shared-theme-provider.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 
+/*  
+  IMPORTANT: In order for shared components to be able to inherit the theme prop, 
+  the shared and site level lcomponents need to be initialized in the same theme provider.
+*/
+
 const SharedThemeProvider = ({ theme, children }) => {
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 };
